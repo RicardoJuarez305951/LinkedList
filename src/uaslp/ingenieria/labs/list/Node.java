fa4d6489e464e643ddba42272cfc138dce9afd1c
RICARDO JUAREZ FLORES <ricardojuarezflores01@gmail.com>
2021-03-01 00:02:27 -0600

    Tarea 5 Linkedlist

--- src/uaslp/ingenieria/labs/list/Node.java
+++ src/uaslp/ingenieria/labs/list/Node.java
@@ -1,34 +1,35 @@
 package uaslp.ingenieria.labs.list;
 
 class Node {
-    //Atributos
-    int dato;
-    Node next;
-    Node previous;
+    private int data;
+    private Node previous;
+    private Node next;
 
-    //Métodos
-
-    public void setDato(int dato) {
-        this.dato = dato;
+    Node(int data){
+        this.data = data;
     }
 
-    public void setNext(Node next) {
-        this.next = next;
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getPrevious() {
+        return previous;
     }
 
     public void setPrevious(Node previous) {
         this.previous = previous;
     }
 
-    public int getDato() {
-        return dato;
-    }
-
     public Node getNext() {
         return next;
     }
 
-    public Node getPrevious() {
-        return previous;
+    public void setNext(Node next) {
+        this.next = next;
     }
 }
